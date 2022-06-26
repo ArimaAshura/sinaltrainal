1fb2cef0834fbf9b8b7874ea4e6ceb426a32aacc
ArimaAshura <frankmateo3@gmail.com>
2022-06-26 18:47:51 -0500

    prueba

---
+++ resources/views/Articulos/agregarArticulo.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row text-center">
+        <h1>
+            Bienvenidos
+        </h1>
+    </div>
+</div>
+@endsection
